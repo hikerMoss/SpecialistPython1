5b51488c5a1331c198b6c09ef2a9f27d56f2b8bd
hikerMoss <106910063+hikerMoss@users.noreply.github.com>
2022-06-11 17:52:30 +0300

    Update 02_hw.py
    Добрый день.
    Задачу вроде бы решил. Вопрос, правильно ли я указал интервалы "между - между"? Можно ли еще упростить код или это достаточно оптимальное решение?

--- Module2/home_work/02_hw.py
+++ Module2/home_work/02_hw.py
@@ -8,3 +8,25 @@
 # Например, 1 корова, 2 коровы, 5 коров, 125 коров.
 
 # TODO: your code here
+
+n = int(input("На лугу пасутся? Укажите число"))
+
+var1 = ("ОШИБКА! УКАЖИТЕ ЧИСЛО БОЛЬШЕ 0!")
+var2 = ("корова")
+var3 = ("коровы")
+var4 = ("коров")
+
+if n < 1:
+    print(var1)
+
+elif n >= 11 and n <= 14:
+    print(n, var4)
+
+elif n == 1 or n % 10 == 1:
+    print(n, var2)
+
+elif n >= 2 and n <= 4 or n % 10 >= 2 and n % 10 <= 4:
+    print(n, var3)
+
+else:
+    print(n, var4)
